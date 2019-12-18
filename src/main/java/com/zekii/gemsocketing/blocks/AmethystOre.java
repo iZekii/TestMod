a26b22f7e04254469197af6f738bb63b14f4aaf5
Zach <zach7505@gmail.com>
2019-12-18 13:10:12 +1100

    made a base class for both ore to inherit from, decreasing redundancy

--- src/main/java/com/zekii/gemsocketing/blocks/AmethystOre.java
+++ src/main/java/com/zekii/gemsocketing/blocks/AmethystOre.java
@@ -1,18 +1,7 @@
 package com.zekii.gemsocketing.blocks;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.SoundType;
-import net.minecraft.block.material.Material;
-import net.minecraftforge.common.ToolType;
-
-public class AmethystOre extends Block {
+public class AmethystOre extends BaseBlock {
     public AmethystOre() {
-        super(Properties.create(Material.ROCK)
-                .sound(SoundType.STONE)
-                .hardnessAndResistance(3.0f)
-                .harvestLevel(3)
-                .harvestTool(ToolType.PICKAXE)
-        );
-        setRegistryName("amethyst_ore");
+        super("amethyst_ore");
     }
 }
---
+++ src/main/java/com/zekii/gemsocketing/blocks/BaseBlock.java
@@ -0,0 +1,51 @@
+package com.zekii.gemsocketing.blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.nbt.ListNBT;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.common.ToolType;
+
+import java.util.Random;
+
+public class BaseBlock extends Block {
+
+    private Random random_gen = new Random();
+    private int xp_amount = random_gen.nextInt(10) + 12;  // Gives 12-22 xp
+
+    public BaseBlock(String name) {
+        super(Properties.create(Material.ROCK)
+                .sound(SoundType.STONE)
+                .hardnessAndResistance(3.0f)
+                .harvestLevel(3)
+                .harvestTool(ToolType.PICKAXE)
+        );
+        setRegistryName(name);
+    }
+    private boolean hasSilkTouch(ItemStack held) {
+        ListNBT enchantments = held.getEnchantmentTagList();
+        for (int i = 0; i < enchantments.size(); i++) {
+            CompoundNBT enchant = enchantments.getCompound(i);
+            if (enchant.getString("id").equals("minecraft:silk_touch")) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public void onBlockHarvested(World world, BlockPos pos, BlockState state, PlayerEntity player) {
+        if (!world.isRemote()){
+            if (!hasSilkTouch(player.getHeldItemMainhand())) {
+                dropXpOnBlockBreak(world, pos, this.xp_amount);
+            }
+        }
+        super.onBlockHarvested(world, pos, state, player);
+    }
+}
